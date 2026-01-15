b50cced82e013dacf30514387f8921d82aedd24b
Dtc-csu <dtccagayanstateuniversitycarig@gmail.com>
2026-01-15 09:34:40 +0800

    Another Try

--- Routes/firebase.js
+++ Routes/firebase.js
@@ -1,5 +1,6 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebase-admin.json");
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_JSON);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
