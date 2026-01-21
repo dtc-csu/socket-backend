cf317ca7491dbae2aba6d780d3efc860674359b5
Dtc-csu <dtccagayanstateuniversitycarig@gmail.com>
2026-01-21 22:22:43 +0800

    Please

--- Routes/familyinfo.js
+++ Routes/familyinfo.js
@@ -4,31 +4,47 @@ const crud = require("../Controllers/genericController");
 const poolPromise = require("../db");
 const controller = crud(poolPromise);
 
-// GET family info by UserID
-router.get("/Users/:id", async (req, res) => {
+// GET family info by PatientID
+router.get("/patient/:id", async (req, res) => {
   try {
     const pool = await poolPromise;
-    const result = await pool.request()
-      .input("userId", req.params.id)
+
+    // First, get PatientIDNoAuto from Patient table using PatientID
+    const patientResult = await pool
+      .request()
+      .input("patientId", req.params.id)
+      .query("SELECT PatientIDNoAuto FROM Patient WHERE PatientID = @patientId");
+
+    if (patientResult.recordset.length === 0) {
+      return res.status(404).json({ message: "Patient not found" });
+    }
+
+    const patientIdNoAuto = patientResult.recordset[0].PatientIDNoAuto;
+
+    // Then, get family info using PatientIDNoAuto
+    const result = await pool
+      .request()
+      .input("patientIdNoAuto", patientIdNoAuto)
       .query(`
         SELECT *
-        FROM UserFamilyInfo
-        WHERE UserID = @userId
+        FROM PatientFamilyInfo
+        WHERE PatientIDNoAuto = @patientIdNoAuto
         ORDER BY CreationDate DESC
       `);
+
     res.json(result.recordset);
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    res.status(500).json({ message: err.message });
   }
 });
 
 // ADD family info
-router.post("/", controller.add("UserFamilyInfo", "FamilyInfoID"));
+router.post("/", controller.add("PatientFamilyInfo", "PatientFamilyInfoId"));
 
 // EDIT family info
-router.put("/:id", controller.edit("UserFamilyInfo", "FamilyInfoID"));
+router.put("/:id", controller.edit("PatientFamilyInfo", "PatientFamilyInfoId"));
 
 // DELETE family info
-router.delete("/:id", controller.delete("UserFamilyInfo", "FamilyInfoID"));
+router.delete("/:id", controller.delete("PatientFamilyInfo", "PatientFamilyInfoId"));
 
 module.exports = router;
